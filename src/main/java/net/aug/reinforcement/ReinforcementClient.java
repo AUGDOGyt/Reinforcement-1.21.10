9b23335567058eee010a47f51756c90c95983eb3
BuildTools <unconfigured@null.spigotmc.org>
2025-10-27 19:25:44 -0500

    e

---
+++ src/main/java/net/aug/reinforcement/ReinforcementClient.java
@@ -0,0 +1,11 @@
+package net.aug.reinforcement;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ReinforcementClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/aug/reinforcement/ReinforcementDataGenerator.java
@@ -0,0 +1,11 @@
+package net.aug.reinforcement;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class ReinforcementDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
