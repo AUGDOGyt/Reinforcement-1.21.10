9678ba15f59bcb48fe220b50b2a69a4f91127e65
BuildTools <unconfigured@null.spigotmc.org>
2025-10-28 21:14:26 -0500

    3 items and a dream

--- src/main/java/net/aug/reinforcement/item/ModItems.java
+++ src/main/java/net/aug/reinforcement/item/ModItems.java
@@ -1,54 +1,80 @@
 package net.aug.reinforcement.item;
 
 import net.aug.reinforcement.Reinforcement;
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroups;
+import net.minecraft.item.*;
+import net.minecraft.item.equipment.ArmorMaterial;
+import net.minecraft.item.equipment.EquipmentAsset;
+import net.minecraft.item.equipment.EquipmentAssetKeys;
+import net.minecraft.item.equipment.EquipmentType;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
+import net.minecraft.registry.tag.BlockTags;
+import net.minecraft.registry.tag.TagKey;
+import net.minecraft.sound.SoundEvents;
+import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
+import org.spongepowered.include.com.google.common.base.Function;
+
+import java.util.Map;
 
 public class ModItems {
 
-    public static final Item REINFORCED_STICK = register("reinforced_stick", Item::new, new Item.Settings());
+	public static final TagKey<Item> REPAIRS_REINFORCED_ARMOR = TagKey.of(Registries.ITEM.getKey(), Identifier.of(Reinforcement.MOD_ID, "repairs_reinforced_armor"));
+	public static final RegistryKey<EquipmentAsset> REINFORCED_ARMOR_MATERIAL_KEY = RegistryKey.of(EquipmentAssetKeys.REGISTRY_KEY, Identifier.of(Reinforcement.MOD_ID, "reinforced_netherite"));
+
+	private static final int BASE_DURABILITY = 12;
+	public static final ArmorMaterial REINFORCED_ARMOR_MATERIAL = new ArmorMaterial(
+			BASE_DURABILITY,
+			Map.of(
+					EquipmentType.HELMET, 3,
+					EquipmentType.CHESTPLATE, 8,
+					EquipmentType.LEGGINGS, 6,
+					EquipmentType.BOOTS, 3
+			),
+			5,
+			SoundEvents.ITEM_ARMOR_EQUIP_IRON,
+			0.0F,
+			0.0F,
+			REPAIRS_REINFORCED_ARMOR,
+			REINFORCED_ARMOR_MATERIAL_KEY
+	);
+
+	public static final ToolMaterial REINFORCED_NETHERITE_TOOL_MATERIAL = new ToolMaterial(
+			BlockTags.INCORRECT_FOR_WOODEN_TOOL,
+			700,
+			5.0F,
+			4.5F,
+			22,
+			REPAIRS_REINFORCED_ARMOR
+	);
+
+
+	public static final Item REINFORCED_STICK = register("reinforced_stick", Item::new, new Item.Settings());
     public static final Item REINFORCED_HANDLE = register("reinforced_handle", Item::new, new Item.Settings());
     public static final Item COMPRESSED_DIAMOND = register("compressed_diamond", Item::new, new Item.Settings());
 
-    public static final Item REINFORCED_NETHERITE_SWORD = register("reinforced_netherite_sword", Item::new, new Item.settings().sword(REINFORCED_NETHERITE_TOOL_MATERIAL, 1.5f, 08.f));
+    public static final Item REINFORCED_NETHERITE_SWORD = register("reinforced_netherite_sword", Item::new, new Item.Settings().sword(REINFORCED_NETHERITE_TOOL_MATERIAL, 1.5f, 08.f));
 
-    public static final ToolMaterial REINFORCED_NETHERITE_TOOL_MATERIAL = new ToolMaterial(
-		BlockTags.INCORRECT_FOR_WOODEN_TOOL,
-		700,
-		5.0F,
-		4.5F,
-		22,
-    );
 
-    public static final ArmorMaterial INSTANCE = new ArmorMaterial(
-		BASE_DURABILITY,
-		Map.of(
-				EquipmentType.HELMET, 3,
-				EquipmentType.CHESTPLATE, 8,
-				EquipmentType.LEGGINGS, 6,
-				EquipmentType.BOOTS, 3
-		),
-		5,
-		SoundEvents.ITEM_ARMOR_EQUIP_IRON,
-		0.0F,
-		0.0F,
-		REPAIRS_GUIDITE_ARMOR,
-		GUIDITE_ARMOR_MATERIAL_KEY
-);
 
     public static final RegistryKey<ItemGroup> CUSTOM_ITEM_GROUP_KEY  = RegistryKey.of(Registries.ITEM_GROUP.getKey(), Identifier.of(Reinforcement.MOD_ID, "reinforcement"));
-    public static final ItemGroup CUSTOM_ITEM_GROUP = FabricItemGroup.builder()
-		.icon(() -> new ItemStack(ModItems.reinforced_stick))
+    public static final ItemGroup CUSTOM_ITEM_GROUP = FabricItemGroup.builder(
+
+			)
+		.icon(() -> new ItemStack(ModItems.REINFORCED_STICK))
 		.displayName(Text.translatable("itemGroup.reinforcement"))
 		.build();
 
-    public static Item register(String name, Function<Item.Settings, Item> itemFactory, Item.Settings settings) {
+
+
+
+	public static Item register(String name, Function<Item.Settings, Item> itemFactory, Item.Settings settings) {
 		// Create the item key.
-		RegistryKey<Item> itemKey = RegistryKey.of(RegistryKeys.ITEM, Identifier.of(ExampleMod.MOD_ID, name));
+		RegistryKey<Item> itemKey = RegistryKey.of(RegistryKeys.ITEM, Identifier.of(Reinforcement.MOD_ID, name));
 
 		// Create the item instance.
 		Item item = itemFactory.apply(settings.registryKey(itemKey));
@@ -61,6 +87,8 @@ public class ModItems {
 
     public static void registerModItems() {
         Reinforcement.LOGGER.info("Registering Mod Items " + Reinforcement.MOD_ID);
+
+		Registry.register(Registries.ITEM_GROUP, CUSTOM_ITEM_GROUP_KEY, CUSTOM_ITEM_GROUP);
 
         ItemGroupEvents.modifyEntriesEvent(CUSTOM_ITEM_GROUP_KEY).register(entries -> {
             entries.add(REINFORCED_STICK);
